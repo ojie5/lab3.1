3a0f223ddda77646d12ff3bb90bbffab9a84d46b
Alexander Eames <P14137988@GWHXX583LIN013W.lec-admin.dmu.ac.uk>
2016-02-02 13:19:01 +0000

    babababab

---
+++ func.c
@@ -0,0 +1,40 @@
+#include <stdio.o>
+
+int addfunc(int a, int b)
+{
+	return a+b;	
+
+}
+		
+//new function
+blalalalalalalala
+
+int divfunc(int a, int b)
+{
+
+
+	return a/b;
+}
+
+int averfunc(int a, int b)
+{
+	return (a+b)/2;
+}
+
+
+
+
+
+int main()
+{
+	int a, b, c;
+
+	printf("enter the number of enteries (max 5, min 1)\n");
+	scanf("%i%i", &a, &b);
+
+	c=averfunc(a, b);
+	printf("%i", c);	
+
+	printf("%i", c);
+
+}
